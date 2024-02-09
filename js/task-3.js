2a155f82ba6501f4c75e16bce5dbfd3c7511166b
JuliaPriadka <150530521+JuliaPriadka@users.noreply.github.com>
2024-02-09 08:27:16 +0200

    Added task-1, task-2 and task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,7 +1,16 @@
-function getElementWidth(content, padding, border) {
-    return parseFloat(content) + parseFloat(padding) * 2 + parseFloat(border) * 2
+
+function filterArray(numbers, value) {
+    const newArray = [];
+    for (let i = 0; i < numbers.length; i++) {
+        if (numbers[i] > value) {
+           newArray.push(numbers[i]);
+         }
+        }
+        return newArray;
 }
 
-console.log(getElementWidth("50px", "8px", "4px")); 
-console.log(getElementWidth("60px", "12px", "8.5px"));
-console.log(getElementWidth("200px", "0px", "0px"));
+console.log(filterArray([1, 2, 3, 4, 5], 3)); 
+console.log(filterArray([1, 2, 3, 4, 5], 4)); 
+console.log(filterArray([1, 2, 3, 4, 5], 5)); 
+console.log(filterArray([12, 24, 8, 41, 76], 38)); 
+console.log(filterArray([12, 24, 8, 41, 76], 20)); 
